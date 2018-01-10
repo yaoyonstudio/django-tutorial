f28d89700484e9803eb0cf9c40fdb25d0e7597eb
yaoyonstudio <yaoyonstudio@gmail.com>
2018-01-10 22:43:51 +0800

    django-tutorial-init

---
+++ mysite/home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse('Hello, django!')
+
+def index(request):
+    context = { 'title': 'Django Tutorial' }
+    return render(request, 'index.html', context)
